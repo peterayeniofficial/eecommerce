a5a45fc2042e006336818d1edd1e92c06898fd5a
Peter Ayeni <ayenicology@gmail.com>
2023-02-09 19:51:37 +0000

    add basketCheckout to API gateway

--- lib/apigateway.ts
+++ lib/apigateway.ts
@@ -16,41 +16,39 @@ export class EcommerceApiGateway extends Construct {
     // Product api gateway
     this.createBasketAPI(props.bascketMicroservice);
   }
-  private createProductAPI(productMicroservice: IFunction){
-    const productAPIGateway = new LambdaRestApi(this, 'productApi', {
-      restApiName: 'Product Service',
+  private createProductAPI(productMicroservice: IFunction) {
+    const productAPIGateway = new LambdaRestApi(this, "productApi", {
+      restApiName: "Product Service",
       handler: productMicroservice,
-      proxy: false
-    })
+      proxy: false,
+    });
 
-    const product = productAPIGateway.root.addResource('product')
-    product.addMethod('GET')
-    product.addMethod('POST')
+    const product = productAPIGateway.root.addResource("product");
+    product.addMethod("GET");
+    product.addMethod("POST");
 
-    const singleProduct = product.addResource('{id}')
-    singleProduct.addMethod('Get')
-    singleProduct.addMethod('PUT')
-    singleProduct.addMethod('DELETE')
-
+    const singleProduct = product.addResource("{id}");
+    singleProduct.addMethod("Get");
+    singleProduct.addMethod("PUT");
+    singleProduct.addMethod("DELETE");
   }
 
-  private createBasketAPI(basketMicroservice: IFunction){
-    const basketAPIGateway = new LambdaRestApi(this, 'basketApi', {
-      restApiName: 'Basket Service',
+  private createBasketAPI(basketMicroservice: IFunction) {
+    const basketAPIGateway = new LambdaRestApi(this, "basketApi", {
+      restApiName: "Basket Service",
       handler: basketMicroservice,
-      proxy: false
-    })
+      proxy: false,
+    });
 
-    const basket = basketAPIGateway.root.addResource('basket')
-    basket.addMethod('GET')
-    basket.addMethod('POST')
+    const basket = basketAPIGateway.root.addResource("basket");
+    basket.addMethod("GET");
+    basket.addMethod("POST");
 
-    const singleBasket = basket.addResource('{username}')
-    singleBasket.addMethod('Get')
-    singleBasket.addMethod('DELETE')
+    const singleBasket = basket.addResource("{username}");
+    singleBasket.addMethod("Get");
+    singleBasket.addMethod("DELETE");
 
-    const basketCheckout = basket.addResource('checkout')
-    basketCheckout.addMethod('POST')
-
+    const basketCheckout = basket.addResource("checkout");
+    basketCheckout.addMethod("POST");
   }
 }
